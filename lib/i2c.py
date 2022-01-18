d481132fe46389dfb5b0c546f28d359013543a4e
Hannah Kolano <hkolano@comcast.net>
2022-01-18 14:28:49 -0800

    time sleep 1 in i2c

--- lib/i2c.py
+++ lib/i2c.py
@@ -1,5 +1,6 @@
 # from .basic import _Basic_class
 from smbus import SMBus
+import time
 
 class I2C(object):
     MASTER = 0
@@ -10,6 +11,7 @@ class I2C(object):
         super().__init__()
         self._bus = 1
         self._smbus = SMBus(self._bus)
+        time.sleep(1)
 
     def _i2c_write_byte(self, addr, data):   # i2C 写系列函数
         # self._debug("_i2c_write_byte: [0x{:02X}] [0x{:02X}]".format(addr, data))
