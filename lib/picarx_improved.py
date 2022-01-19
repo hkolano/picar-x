83a08a2b7df432ba0f3ddeee8aa3d7b42b7764a5
Hannah Kolano <hkolano@comcast.net>
2022-01-18 18:05:18 -0800

    moved location of config file

--- lib/picarx_improved.py
+++ lib/picarx_improved.py
@@ -43,7 +43,7 @@ class Picarx(object):
         self.dir_servo_pin = Servo(PWM('P2'))
         self.camera_servo_pin1 = Servo(PWM('P0'))
         self.camera_servo_pin2 = Servo(PWM('P1'))
-        self.config_flie = fileDB('/home/pi/.config')
+        self.config_flie = fileDB('/home/hannah/.config') # RENAME IF SWITCH USERNAME
         self.dir_cal_value = int(self.config_flie.get("picarx_dir_servo", default_value=0))
         self.cam_cal_value_1 = int(self.config_flie.get("picarx_cam1_servo", default_value=0))
         self.cam_cal_value_2 = int(self.config_flie.get("picarx_cam2_servo", default_value=0))
