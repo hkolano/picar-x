a28b5936fd0d06c15b5babbe80dc9b4ad0dc8f10
Hannah Kolano <hkolano@comcast.net>
2022-01-19 13:40:27 -0800

    grayscale threshold calibration take 1

--- lib/sensing.py
+++ lib/sensing.py
@@ -1,3 +1,4 @@
+from statistics import mean
 from picarx_improved import Picarx
 
 class Sensing():
@@ -25,17 +26,39 @@ class Interpreter():
         Inputs:
         sensor: the Sensing object
         sensitivity: how different light & dark readings are
-        polarity: 1 - line is darker; 2 - line is lighter'''
+        polarity: 1 - line is lighter; -1 - line is darker'''
         self.sensor = sensor
         self.sensitivity = sensitivity
         self.polarity = polarity
 
     def calibrate(self):
-        input("Place the sensor over the floor.")
+        # Get grayscale values for the floor and the line
+        input("Place the sensor over the floor and hit enter.")
         floor_data = self.sensor.get_grayscale_data()
-        input("Place the sensor over the line.")
+        floor_avg = mean(floor_data)
+        input("Place the sensor over the line and hit enter.")
         line_data = self.sensor.get_grayscale_data()
-        input("Place the sensor centered on the line.")
-        middle_data = self.sensor.get_grayscale_data()
-        print("floor: ", floor_data, "\n line: ", line_data, "\n middle: ", middle_data)
+        line_avg = mean(line_data)
+        print("floor: ", floor_data, "\n line: ", line_data)
+
+        # Define polarity based on which had more light
+        light_diff = line_avg - floor_avg
+        if light_diff > 0:
+            self.polarity = 1
+        else:
+            self.polarity = -1
+
+        # Define threshold based on difference
+        one_third_range = abs(light_diff)/3
+        if self.polarity == 1:
+            self.thresh_close = line_avg - one_third_range
+            self.thresh_far = floor_avg + one_third_range
+        if self.polarity == -1:
+            self.thresh_close = line_avg + one_third_range
+            self.thresh_far = floor_avg - one_third_range
+
+        print("polarity is: ", self.polarity)
+        print("Close threshold: ", self.thresh_close, "Far threshold: ", self.thresh_far)
+        
+
 
