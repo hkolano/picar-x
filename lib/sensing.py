c94d5d0888b5da773743be998bfdb43ed7623b33
Hannah Kolano <hkolano@comcast.net>
2022-01-19 16:47:54 -0800

    added comments

--- lib/sensing.py
+++ lib/sensing.py
@@ -27,8 +27,8 @@ class Interpreter():
         Inputs:
         sensor: the Sensing object
         polarity: 1 - line is lighter; -1 - line is darker
-        thresh_close: more extreme than thresh_close = line
-        thresh_far: more extreme than thresh_far = floor'''
+        thresh_close: more extreme than thresh_close indicates the line
+        thresh_far: more extreme than thresh_far indicates the floor'''
         self.sensor = sensor
         self.thresh_close = thresh_close
         self.thresh_far = thresh_far
@@ -38,10 +38,10 @@ class Interpreter():
         ''' Run to set self.thresh values and self.polarity via 
         a calibration routine with the user. '''
         # Get grayscale values for the floor and the line
-        input("Place the sensor over the floor and hit enter.")
+        input("Place all 3 sensors over the floor and hit enter.")
         floor_data = self.sensor.get_grayscale_data()
         floor_avg = mean(floor_data)
-        input("Place the sensor over the line and hit enter.")
+        input("Place all 3 sensors over the line and hit enter.")
         line_data = self.sensor.get_grayscale_data()
         line_avg = mean(line_data)
         print("floor: ", floor_data, "\n line: ", line_data)
@@ -53,7 +53,7 @@ class Interpreter():
         else:
             self.polarity = -1
 
-        # Define threshold based on difference
+        # Define thresholds based on difference
         one_third_range = abs(light_diff)/3
         if self.polarity == 1:
             self.thresh_close = line_avg - one_third_range
@@ -69,9 +69,9 @@ class Interpreter():
         ''' Interprets grayscale data and returns a guess as to where
          the robot is in reference to the line.
         Returns:
-        position: number on interval [-1 1] (positive = too far left) 
-        indicating position of robot with respect to line'''
+        pos: number on interval [-1 1] (positive = robot is too far right)'''
         closeness_vector = []
+        pos = 0
         if self.polarity == 1:
             for val in gray_data:
                 if val < self.thresh_far:
@@ -84,23 +84,31 @@ class Interpreter():
         # if center sensor is over line
         if closeness_vector[1] == 3:
             if closeness_vector[0] == closeness_vector[2]:
+                pos = 0
                 print("Centered!")
             elif closeness_vector[0] > closeness_vector[2]:
-                print("slightly right???")
+                pos = 0.33
+                print("slightly right...")
             elif closeness_vector[0] < closeness_vector[2]:
-                print("slightly left??")
+                pos = -0.33
+                print("slightly left...")
         # if center sensor is slightly off the line
         elif closeness_vector[1] == 2:
             if closeness_vector[0] > 1:
+                pos = 0.67
                 print("Pretty right")
             elif closeness_vector[2] > 1:
+                pos = -0.67
                 print("Pretty left")
-            else:
-                print("no idea. Vector is ", closeness_vector)
+        # if center sensor is completely off the line
         elif closeness_vector[1] == 1:
             if closeness_vector[0] > 1:
+                pos = 1.0
                 print("VERY RIGHT")
             elif closeness_vector[2] > 1:
+                pos = -1.0
                 print("VERY LEFT")
         else:
-            print("wtf how did you get here")
+            print("wtf how did you get here? vec is ", closeness_vector)
+
+        return pos
